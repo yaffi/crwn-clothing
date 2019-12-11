1be3aa05d8cbe23522dad90aed2b5d79ad32ba7a
yaffi <gkmsnu.14.calm@gmail.com>
2019-12-12 06:45:50 +0900

    menu-itemにwithRouterを導入し、更新

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
+const Hats = () => (
+  <div>
+    HatsPage
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path='/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -8,37 +8,42 @@ class Directory extends React.Component {
     constructor() {
         super();
 
-        this.state={
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 2
-                },
-                {
-                    title: 'snekers',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 3
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 4,
-                    size: 'large'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 5,
-                    size: 'large'
-                }
-            ]
-        }
+        this.state = {
+          sections: [
+            {
+              title: "hats",
+              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+              id: 1,
+              linkUrl: "hats"
+            },
+            {
+              title: "jackets",
+              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+              id: 2,
+              linkUrl: "hats"
+            },
+            {
+              title: "snekers",
+              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+              id: 3,
+              linkUrl: "hats"
+            },
+            {
+              title: "mens",
+              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+              id: 4,
+              size: "large",
+              linkUrl: "hats"
+            },
+            {
+              title: "womens",
+              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+              id: 5,
+              size: "large",
+              linkUrl: "hats"
+            }
+          ]
+        };
     }
 
     render() {
