0300b8f090da20f3d4bdbb2cf9514a80a46d0cf1
yaffi <gkmsnu.14.calm@gmail.com>
2019-12-11 22:00:09 +0900

    add project

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,7 +7,7 @@ const Homepage = () => (
         <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>HATS</h1>
+                    <h1 className='title'>HAT</h1>
                     <span className='subtitle'> SHOP NOW </span>
                 </div>
             </div>
